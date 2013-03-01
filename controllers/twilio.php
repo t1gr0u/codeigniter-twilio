c5efa9d6ed5c1e0ab78265cf85e067c85f15c74f
t1gr0u <tigrou.m@gmail.com>
2013-03-01 15:31:22 +0000

    added some comments and clean up the code

--- controllers/twilio.php
+++ controllers/twilio.php
@@ -12,23 +12,21 @@ class Twilio extends CI_Controller {
 
   function __construct() {
     try {
+      // load the library
+      $this->load->library( 'twilio_services' );
+      $thisMonth = date( 'Y-m-d', mktime( 3, 0, 0, date( 'm' ), 1, date( 'Y' ) ) );
 
-        $this->load->library( 'twilio_services' );
-        $thisMonth = date( 'Y-m-d', mktime( 3, 0, 0, date( 'm' ), 1, date( 'Y' ) ) );
-
-        foreach ( $this->twilio_services->account->calls->getIterator( 0, 50, array(
-            "Status" => "completed",
-            "StartTime>" => $thisMonth
-          ) ) as $call
-        ) {
-          print ' $call->direction: ' . $call->direction . ' date_created:' . $call->date_created . ' $call->from: ' . $call->from . ' $call->to: ' . $call->to . '<br/>';
-        }
-
-
+      // get all the call from the 1st of the month
+      foreach ( $this->twilio_services->account->calls->getIterator( 0, 50, array(
+          "Status" => "completed",
+          "StartTime>" => $thisMonth
+        ) ) as $call
+      ) {
+        print ' $call->direction: ' . $call->direction . ' date_created:' . $call->date_created . ' $call->from: ' . $call->from . ' $call->to: ' . $call->to . '<br/>';
+      }
     } catch( Exception $e ) {
       print $e->getMessage();
     }
-
   }
 }
 
